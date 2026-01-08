56737a37769299ec3e0285938ffe90bcf3bb0e36
6607changchun <luobojiaozi@163.com>
2026-01-08 10:54:11 +0800

    feat: signature-related types.

--- electron/kg/reftype.cjs
+++ electron/kg/reftype.cjs
@@ -1,1 +1,7 @@
 const subjectType = require("./reftype/subject.cjs")
+const signatureType = require("./reftype/signature.cjs")
+
+module.exports = {
+    subjectType,
+    signatureType
+}
---
+++ electron/kg/reftype/signature.cjs
@@ -0,0 +1,76 @@
+const kg_interface = require("../interface.cjs")
+const { primitive, AttrReq } = require("../predefine.cjs")
+
+//Authors
+class AttributeSigName extends kg_interface.Attribute {
+    static #instance = new AttributeSigName()
+    static #first_create = true
+    constructor(){
+        if(!AttributeSigName.#first_create){
+            throw new TypeError("AttributeSigName is not constructable, use AttributeSigName.instance instead.")
+        }
+        AttributeSigName.#first_create = false
+        super(AttrReq.required.value, "AuthorName", primitive.StringType.instance.id)
+    }
+    static get instance(){return AttributeSigName.#instance}
+}
+
+class Author extends kg_interface.Concept{
+    static #instance = new Author()
+    static #first_create = true
+    constructor(){
+        if(!Author.#first_create){
+            throw new TypeError("Author is not constructable, use Author.instance instead.")
+        }
+        Author.#first_create = false
+        super([AttributeSigName.instance.id], "author")
+    }
+    static get instance(){return Author.#instance}
+}
+
+class Affiliation extends kg_interface.Concept {
+    static #instance = new Affiliation()
+    static #first_create = true
+    constructor(){
+        if(!Affiliation.#first_create){
+            throw new TypeError("Affiliation is not constructable, use Affiliation.instance instead.")
+        }
+        Affiliation.#first_create = false
+        super([AttributeSigName.instance.id], "affiliation")
+    }
+    static get instance(){return Affiliation.#instance}
+}
+
+class AuthorBelongTo extends kg_interface.InstanceRel{
+    static #instance = new AuthorBelongTo()
+    static #first_create = true
+    constructor(){
+        if(!AuthorBelongTo.#first_create){
+            throw new TypeError("AuthorBelongTo is not constructable, use AuthorBelongTo.instance instead.")
+        }
+        AuthorBelongTo.#first_create = false
+        super("authorBelongTo", Author.instance.id, Affiliation.instance.id)
+    }
+    static get instance(){return AuthorBelongTo.#instance}
+}
+
+class AffiliationBelongTo extends kg_interface.InstanceRel{
+    static #instance = new AffiliationBelongTo()
+    static #first_create = true
+    constructor(){
+        if(!AffiliationBelongTo.#first_create){
+            throw new TypeError("AffiliationBelongTo is not constructable, use AffiliationBelongTo.instance instead.")
+        }
+        AffiliationBelongTo.#first_create = false
+        super("affiliationBelongTo", Affiliation.instance.id, Affiliation.instance.id)
+    }
+    static get instance(){return AffiliationBelongTo.#instance}
+}
+
+module.exports = {
+    AttributeSigName,
+    Author,
+    Affiliation,
+    AuthorBelongTo,
+    AffiliationBelongTo
+}
