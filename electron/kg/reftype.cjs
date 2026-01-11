99f2beb6e5748ab8a2bcaaf7bb4a21378d447a13
6607changchun <luobojiaozi@163.com>
2026-01-11 19:10:22 +0800

    feat: add tech-related types.

--- electron/kg/reftype.cjs
+++ electron/kg/reftype.cjs
@@ -1,7 +1,9 @@
 const subjectType = require("./reftype/subject.cjs")
 const signatureType = require("./reftype/signature.cjs")
+const evolutionType = require("./reftype/evolution.cjs")
 
 module.exports = {
     subjectType,
-    signatureType
+    signatureType,
+    evolutionType
 }
