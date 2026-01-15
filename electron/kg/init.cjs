436e748822637d0a747bc7c2e849a8b41e71381f
6607changchun <luobojiaozi@163.com>
2026-01-15 20:56:37 +0800

    feat(WIP): add kg_type init after db is finished.

---
+++ electron/kg/init.cjs
@@ -0,0 +1,9 @@
+const refType = require("./reftype.cjs")
+const predefine = require("./predefine.cjs")
+const kg_interface = require("./interface.cjs")
+
+// TODO: after db
+
+module.exports = {
+
+}
