522a3ce87c76b9f863ac81712834cc0150ab6678
6607changchun <luobojiaozi@163.com>
2026-01-15 20:49:06 +0800

    feat: article-related types.

--- electron/kg/reftype.cjs
+++ electron/kg/reftype.cjs
@@ -1,9 +1,11 @@
 const subjectType = require("./reftype/subject.cjs")
 const signatureType = require("./reftype/signature.cjs")
 const evolutionType = require("./reftype/evolution.cjs")
+const articleType = require("./reftype/article.cjs")
 
 module.exports = {
     subjectType,
     signatureType,
-    evolutionType
+    evolutionType,
+    articleType
 }
---
+++ electron/kg/reftype/article.cjs
@@ -0,0 +1,259 @@
+const kg_interface = require("../interface.cjs")
+const { AttrReq, primitive, constraints } = require("../predefine.cjs")
+const kg_evolution = require("./evolution.cjs")
+const kg_signature = require("./signature.cjs")
+
+// Article/Refentry
+class AttributeArtTitle extends kg_interface.Attribute {
+    static #instance = new AttributeArtTitle()
+    static #first_create = true
+    constructor(){
+        if(!AttributeArtTitle.#first_create){
+            throw new TypeError("AttributeArtTitle is not constructable, use AttributeArtTitle.instance instead.")
+        }
+        AttributeArtTitle.#first_create = false
+        super(AttrReq.required.value, "artTitle", primitive.StringType.instance.id)
+    }
+    static get instance(){return AttributeArtTitle.#instance}
+}
+class AttributeArtPrimaryRefEntry extends kg_interface.Attribute {
+    static #instance = new AttributeArtPrimaryRefEntry()
+    static #first_create = true
+    constructor(){
+        if(!AttributeArtPrimaryRefEntry.#first_create){
+            throw new TypeError("AttributeArtPrimaryRefEntry is not constructable, use AttributeArtPrimaryRefEntry.instance instead.")
+        }
+        AttributeArtPrimaryRefEntry.#first_create = false
+        super(AttrReq.optional.value, "artPrimaryRefEntry", primitive.NumberType.instance.id)
+    }
+    static get instance(){return AttributeArtPrimaryRefEntry.#instance}
+}
+
+class AttributeRefNo extends kg_interface.Attribute {
+    static #instance = new AttributeRefNo()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefNo.#first_create){
+            throw new TypeError("AttributeRefNo is not constructable, use AttributeRefNo.instance instead.")
+        }
+        AttributeRefNo.#first_create = false
+        super(AttrReq.required.value, "refNo", primitive.NumberType.instance.id)
+    }
+    static get instance(){return AttributeRefNo.#instance}
+}
+class AttributeRefTitle extends kg_interface.Attribute {
+    static #instance = new AttributeRefTitle()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefTitle.#first_create){
+            throw new TypeError("AttributeRefTitle is not constructable, use AttributeRefTitle.instance instead.")
+        }
+        AttributeRefTitle.#first_create = false
+        super(AttrReq.optional.value, "refTitle", primitive.StringType.instance.id)
+    }
+    static get instance(){return AttributeRefTitle.#instance}
+}
+class AttributeRefYear extends kg_interface.Attribute {
+    static #instance = new AttributeRefYear()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefYear.#first_create){
+            throw new TypeError("AttributeRefYear is not constructable, use AttributeRefYear.instance instead.")
+        }
+        AttributeRefYear.#first_create = false
+        super(AttrReq.optional.value, "refYear", primitive.NumberType.instance.id)
+    }
+    static get instance(){return AttributeRefYear.#instance}
+}
+class AttributeRefPublication extends kg_interface.Attribute {
+    static #instance = new AttributeRefPublication()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefPublication.#first_create){
+            throw new TypeError("AttributeRefPublication is not constructable, use AttributeRefPublication.instance instead.")
+        }
+        AttributeRefPublication.#first_create = false
+        super(AttrReq.required.value, "refPublication", primitive.StringType.instance.id)
+    }
+    static get instance(){return AttributeRefPublication.#instance}
+}
+class AttributeRefIndex extends kg_interface.Attribute {
+    static #instance = new AttributeRefIndex()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefIndex.#first_create){
+            throw new TypeError("AttributeRefIndex is not constructable, use AttributeRefIndex.instance instead.")
+        }
+        AttributeRefIndex.#first_create = false
+        super(AttrReq.optional.value, "refIndex", primitive.StringType.instance.id)
+    }
+    static get instance(){return AttributeRefIndex.#instance}
+}
+class AttributeRefVolume extends kg_interface.Attribute {
+    static #instance = new AttributeRefVolume()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefVolume.#first_create){
+            throw new TypeError("AttributeRefVolume is not constructable, use AttributeRefVolume.instance instead.")
+        }
+        AttributeRefVolume.#first_create = false
+        super(AttrReq.optional.value, "refVolume", primitive.NumberType.instance.id)
+    }
+    static get instance(){return AttributeRefVolume.#instance}
+}
+class AttributeRefIssue extends kg_interface.Attribute {
+    static #instance = new AttributeRefIssue()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefIssue.#first_create){
+            throw new TypeError("AttributeRefIssue is not constructable, use AttributeRefIssue.instance instead.")
+        }
+        AttributeRefIssue.#first_create = false
+        super(AttrReq.conditionChild.value, "refIssue", primitive.NumberType.instance.id)
+    }
+    static get instance(){return AttributeRefIssue.#instance}
+}
+class AttributeRefStartPage extends kg_interface.Attribute {
+    static #instance = new AttributeRefStartPage()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefStartPage.#first_create){
+            throw new TypeError("AttributeRefStartPage is not constructable, use AttributeRefStartPage.instance instead.")
+        }
+        AttributeRefStartPage.#first_create = false
+        super(AttrReq.optional.value, "refStartPage", primitive.NumberType.instance.id)
+    }
+    static get instance(){return AttributeRefStartPage.#instance}
+}
+class AttributeRefEndPage extends kg_interface.Attribute {
+    static #instance = new AttributeRefEndPage()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefEndPage.#first_create){
+            throw new TypeError("AttributeRefEndPage is not constructable, use AttributeRefEndPage.instance instead.")
+        }
+        AttributeRefEndPage.#first_create = false
+        super(AttrReq.required.value, "refEndPage", primitive.NumberType.instance.id)
+    }
+    static get instance(){return AttributeRefEndPage.#instance}
+}
+class AttributeRefDoi extends kg_interface.Attribute {
+    static #instance = new AttributeRefDoi()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefDoi.#first_create){
+            throw new TypeError("AttributeRefDoi is not constructable, use AttributeRefDoi.instance instead.")
+        }
+        AttributeRefDoi.#first_create = false
+        super(AttrReq.optional.value, "refDoi", primitive.StringType.instance.id)
+    }
+    static get instance(){return AttributeRefDoi.#instance}
+}
+class AttributeRefAbstract extends kg_interface.Attribute {
+    static #instance = new AttributeRefAbstract()
+    static #first_create = true
+    constructor(){
+        if(!AttributeRefAbstract.#first_create){
+            throw new TypeError("AttributeRefAbstract is not constructable, use AttributeRefAbstract.instance instead.")
+        }
+        AttributeRefAbstract.#first_create = false
+        super(AttrReq.required.value, "refAbs", primitive.StringType.instance.id)
+    }
+    static get instance(){return AttributeRefAbstract.#instance}
+}
+
+class Article extends kg_interface.Concept{
+    static #instance = new Article()
+    static #first_create = true
+    constructor(){
+        if(!Article.#first_create){
+            throw new TypeError("Article is not constructable, use Article.instance instead.")
+        }
+        Article.#first_create = false
+        super([AttributeArtTitle.instance.id, AttributeArtPrimaryRefEntry.instance.id], "article")
+    }
+    static get instance(){return Article.#instance}
+}
+class Reference extends kg_interface.Concept{
+    static #instance = new Reference()
+    static #first_create = true
+    constructor(){
+        if(!Reference.#first_create){
+            throw new TypeError("Reference is not constructable, use Reference.instance instead.")
+        }
+        Reference.#first_create = false
+        super(
+            [
+                AttributeRefNo.instance.id, AttributeRefIndex.instance.id, AttributeRefTitle.instance.id,
+                AttributeRefPublication.instance.id, AttributeRefYear.instance.id, AttributeRefVolume.instance.id, AttributeRefIssue.instance.id,
+                AttributeRefStartPage.instance.id, AttributeRefEndPage.instance.id, AttributeRefDoi.instance.id,
+                AttributeRefAbstract.instance.id
+            ],
+            "reference"
+        )
+    }
+    static get instance(){return Reference.#instance}
+}
+
+// atricle -> ref
+// atricle -> EvoEntity
+// ref -> author
+// atricle -> contribution
+class ArticleRefEntry extends kg_interface.InstanceRel{
+    static #instance = new ArticleRefEntry()
+    static #first_create = true
+    constructor(){
+        if(!ArticleRefEntry.#first_create){
+            throw new TypeError("ArticleRefEntry is not constructable, use ArticleRefEntry.instance instead.")
+        }
+        ArticleRefEntry.#first_create = false
+        super("articleRefEntry", Article.instance.id, Reference.instance.id)
+    }
+    static get instance(){return ArticleRefEntry.#instance}
+}
+class ArticleTag extends kg_interface.InstanceRel{
+    static #instance = new ArticleTag()
+    static #first_create = true
+    constructor(){
+        if(!ArticleTag.#first_create){
+            throw new TypeError("ArticleTag is not constructable, use ArticleTag.instance instead.")
+        }
+        ArticleTag.#first_create = false
+        super("articleTag", Article.instance.id, kg_evolution.evoConcepts.EvoEntity.instance.id)
+    }
+    static get instance(){return ArticleTag.#instance}
+}
+class RefAuthor extends kg_interface.InstanceRel{
+    static #instance = new RefAuthor()
+    static #first_create = true
+    constructor(){
+        if(!RefAuthor.#first_create){
+            throw new TypeError("RefAuthor is not constructable, use RefAuthor.instance instead.")
+        }
+        RefAuthor.#first_create = false
+        super("refAuthor", Reference.instance.id, kg_signature.Author.instance.id)
+    }
+    static get instance(){return RefAuthor.#instance}
+}
+class ArticleContrib extends kg_interface.InstanceRel{
+    static #instance = new ArticleContrib()
+    static #first_create = true
+    constructor(){
+        if(!ArticleContrib.#first_create){
+            throw new TypeError("ArticleContrib is not constructable, use ArticleContrib.instance instead.")
+        }
+        ArticleContrib.#first_create = false
+        super("articleContrib", Article.instance.id, kg_evolution.evoConcepts.EvoContrib.instance.id)
+    }
+    static get instance(){return ArticleContrib.#instance}
+}
+
+module.exports = {
+    AttributeArtTitle, AttributeArtPrimaryRefEntry,
+    AttributeRefNo, AttributeRefIndex, AttributeRefTitle,
+    AttributeRefAbstract, AttributeRefYear, AttributeRefVolume, AttributeRefIssue,
+    AttributeRefStartPage, AttributeRefEndPage, AttributeRefDoi,
+    AttributeRefAbstract,
+    Article, Reference,
+    ArticleRefEntry, ArticleContrib, ArticleTag, RefAuthor
+}
