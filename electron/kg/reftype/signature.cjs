d2c1e4208a75622b0a33435c670142916a3ed846
6607changchun <luobojiaozi@163.com>
2026-01-24 10:14:13 +0800

    feat: add signature of article.

--- electron/kg/reftype/signature.cjs
+++ electron/kg/reftype/signature.cjs
@@ -41,6 +41,19 @@ class Affiliation extends kg_interface.Concept {
     static get instance(){return Affiliation.#instance}
 }
 
+class Signature extends kg_interface.Concept{
+    static #first_create = true
+    static #instance = new Signature()
+    constructor(){
+        if(!Signature.#first_create){
+            throw new TypeError("Signature is not constructable, use Signature.instance instead.")
+        }
+        Signature.#first_create = false
+        super([AttributeSigName.instance.id], "signature")
+    }
+    static get instance(){return Signature.#instance}
+}
+
 class AuthorBelongTo extends kg_interface.InstanceRel{
     static #first_create = true
     static #instance = new AuthorBelongTo()
@@ -67,10 +80,39 @@ class AffiliationBelongTo extends kg_interface.InstanceRel{
     static get instance(){return AffiliationBelongTo.#instance}
 }
 
+class SignatureAuthor extends kg_interface.InstanceRel{
+    static #first_create = true
+    static #instance = new SignatureAuthor()
+    constructor(){
+        if(!SignatureAuthor.#first_create){
+            throw new TypeError("SignatureAuthor is not constructable, use SignatureAuthor.instance instead.")
+        }
+        SignatureAuthor.#first_create = false
+        super("signatureAuthor", Signature.instance.id, Author.instance.id)
+    }
+    static get instance(){return SignatureAuthor.#instance}
+}
+
+class SignatureAffiliation extends kg_interface.InstanceRel{
+    static #first_create = true
+    static #instance = new SignatureAffiliation()
+    constructor(){
+        if(!SignatureAffiliation.#first_create){
+            throw new TypeError("SignatureAffiliation is not constructable, use SignatureAffiliation.instance instead.")
+        }
+        SignatureAffiliation.#first_create = false
+        super("signatureAffiliation", Signature.instance.id, Affiliation.instance.id)
+    }
+    static get instance(){return SignatureAffiliation.#instance}
+}
+
 module.exports = {
     AttributeSigName,
     Author,
+    Signature,
     Affiliation,
     AuthorBelongTo,
-    AffiliationBelongTo
+    AffiliationBelongTo,
+    SignatureAuthor,
+    SignatureAffiliation
 }
